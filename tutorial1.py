dd025382a7410a5e44639346326c55f6925b80a4
Alex McCullough <alexmccullough@outlook.com>
2024-09-11 14:11:33 +0100

    feat: colour testing

--- tutorial1.py
+++ tutorial1.py
@@ -2,10 +2,14 @@ import curses
 from curses import wrapper
 
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_YELLOW)
+    curses.init_pair(2, curses.COLOR_MAGENTA, curses.COLOR_WHITE)
+    BLUE_AND_YELLOW = curses.color_pair(1)
+    MAGENTA_AND_WHITE = curses.color_pair(2)
+
     stdscr.clear()
-    stdscr.addstr(25, 40, "hello world", curses.A_UNDERLINE)
-    stdscr.addstr(15, 60, "goodbye world")
-    stdscr.addstr(15, 55, "overwritten")
+    stdscr.addstr(25, 40, "hello world", BLUE_AND_YELLOW | curses.A_UNDERLINE)
+    stdscr.addstr(15, 60, "goodbye world", MAGENTA_AND_WHITE | curses.A_REVERSE)
     stdscr.refresh()
     stdscr.getch()
 
