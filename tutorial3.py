96633e8d371c1eac323331c1ec871152b2a847b3
Alex McCullough <alexmccullough@outlook.com>
2024-09-11 14:36:05 +0100

    feat: window demo

--- tutorial3.py
+++ tutorial3.py
@@ -8,15 +8,21 @@ def main(stdscr):
     BLUE_AND_BLACK = curses.color_pair(1)
     RED_AND_BLACK = curses.color_pair(2)
 
+    counter_win = curses.newwin(1, 20, 10, 10)
+    stdscr.addstr("hello world!")
+    stdscr.refresh()
+
     for i in range(100):
-        stdscr.clear()
+        counter_win.clear()
         color = BLUE_AND_BLACK
 
         if i % 2 == 0:
             color = RED_AND_BLACK
-        stdscr.addstr(f"Count: {i}", color)
-        stdscr.refresh()
+
+        counter_win.addstr(f"Count: {i}", color)
+        counter_win.refresh()
         time.sleep(0.1)
+
     stdscr.getch()
 
 wrapper(main)
